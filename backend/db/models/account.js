8afd08dd0353173b22ca7e32ad9fb978dc0756a3
Joshua <j.maxwell.alan@gmail.com>
2021-07-23 18:19:19 -0400

    create account model

---
+++ backend/db/models/account.js
@@ -0,0 +1,34 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, { DataTypes, fn }) => {
+  class Account extends Model {
+    static associate ({ User, Item }) {
+      Account.belongsTo(User, { foreignKey: 'userId' });
+      Account.hasMany(Item, { foreignKey: 'accountId' });
+    }
+  }
+
+  Account.init({
+    balance: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users'
+      }
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    },
+    udpatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    }
+  }, {
+    sequelize,
+    modelName: 'Account'
+  });
+
+  return Account;
+};
