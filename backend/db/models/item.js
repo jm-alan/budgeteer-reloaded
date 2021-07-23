e10534caca8a26cab57d75e5c90b8abadd79dba5
Joshua <j.maxwell.alan@gmail.com>
2021-07-23 17:27:30 -0400

    create item model

---
+++ backend/db/models/item.js
@@ -0,0 +1,40 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, { DataTypes, fn }) => {
+  class Item extends Model {
+    static associate ({ User }) {
+      Item.belongsTo(User, { foreignKey: 'userId' });
+    }
+  }
+
+  Item.init({
+    recurring: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    income: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    amount: DataTypes.STRING,
+    effectiveDate: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    },
+    udpatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    }
+  }, {
+    sequelize,
+    modelName: 'Item'
+  });
+};
