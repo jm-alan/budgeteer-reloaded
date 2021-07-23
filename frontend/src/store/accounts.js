4f0313499189dfd42557c3b6abf868bb761ef9b4
Joshua <j.maxwell.alan@gmail.com>
2021-07-23 19:52:45 -0400

    create basic account load/unload actions

--- frontend/src/store/accounts.js
+++ frontend/src/store/accounts.js
@@ -1,8 +1,42 @@
+import csrfetch from './csrfetch';
+
+const LOAD_ALL = 'accounts/ALL';
+const UNLOAD = 'accounts/UNLOAD';
+
+const setAccounts = accounts => ({
+  type: LOAD_ALL,
+  accounts
+});
+
+export const GetAccounts = () => async dispatch => {
+  const { accounts } = await csrfetch.get('/api/accounts/');
+
+  dispatch(setAccounts(accounts));
+};
+
+export const UnloadAccounts = () => ({
+  type: UNLOAD
+});
+
 export default function reducer (
-  state = { all: {}, current: null },
-  { type }
+  state = { all: {}, current: null, loaded: false },
+  { type, accounts }
 ) {
   switch (type) {
+    case LOAD_ALL:
+      return {
+        ...state,
+        all: {
+          ...accounts
+        },
+        loaded: true
+      };
+    case UNLOAD:
+      return {
+        ...state,
+        all: {},
+        loaded: false
+      };
     default:
       return state;
   }
