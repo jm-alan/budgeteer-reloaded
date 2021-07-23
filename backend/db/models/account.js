5889c90e2da6d2de79249cdccf7836615f669f22
Joshua <j.maxwell.alan@gmail.com>
2021-07-23 18:43:47 -0400

    typo

--- backend/db/models/account.js
+++ backend/db/models/account.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, { DataTypes, fn }) => {
       type: DataTypes.DATE,
       defaultValue: fn('now')
     },
-    udpatedAt: {
+    updatedAt: {
       type: DataTypes.DATE,
       defaultValue: fn('now')
     }
