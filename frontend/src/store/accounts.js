51371b355033ff4e1eb607ca17ef9897e19b0d69
Joshua <j.maxwell.alan@gmail.com>
2021-07-25 14:22:30 -0400

    create full CRUD actions for account store

--- frontend/src/store/accounts.js
+++ frontend/src/store/accounts.js
@@ -2,16 +2,48 @@ import csrfetch from './csrfetch';
 
 const LOAD_ALL = 'accounts/ALL';
 const UNLOAD = 'accounts/UNLOAD';
+const CREATE = 'accounts/CREATE';
+const UPDATE = 'accounts/UPDATE';
+const DELETE = 'accounts/DELETE';
 
 const setAccounts = accounts => ({
   type: LOAD_ALL,
   accounts
 });
 
+const createAccount = account => ({
+  type: CREATE,
+  account
+});
+
+const updateAccount = account => ({
+  type: UPDATE,
+  account
+});
+
+const deleteAccount = accountId => ({
+  type: DELETE,
+  accountId
+});
+
 export const GetAccounts = () => async dispatch => {
   const { accounts } = await csrfetch.get('/api/accounts/');
-
   dispatch(setAccounts(accounts));
+};
+
+export const CreateAccount = newAccount => async dispatch => {
+  const { account } = await csrfetch.post('/api/accounts/', newAccount);
+  dispatch(createAccount(account));
+};
+
+export const UpdateAccount = (accountId, data) => async dispatch => {
+  const { account } = await csrfetch.patch(`/api/accounts/${accountId}/`, data);
+  dispatch(updateAccount(account));
+};
+
+export const DeleteAccount = accountId => async dispatch => {
+  await csrfetch.delete(`/api/accounts/${accountId}/`);
+  dispatch(deleteAccount(accountId));
 };
 
 export const UnloadAccounts = () => ({
