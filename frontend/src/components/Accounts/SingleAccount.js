7fba6dc74aaf0447df9b22295dab8fff6980aacc
Joshua <j.maxwell.alan@gmail.com>
2021-07-25 16:53:05 -0400

    navigate to account on click

--- frontend/src/components/Accounts/SingleAccount.js
+++ frontend/src/components/Accounts/SingleAccount.js
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router';
+
 import HotswapInput from '../UsefulTools/HotswapInput';
 import { UpdateAccount } from '../../store/accounts';
 import { useHotswap } from '../../utils/hooks';
@@ -5,13 +7,22 @@ import { useHotswap } from '../../utils/hooks';
 import './index.css';
 
 export default function SingleAccount ({ account }) {
+  const history = useHistory();
+
   const [name, hotswapSetName, hotswapSubmitName] =
     useHotswap('name', account.name, UpdateAccount, account.id);
 
+  const goToAccount = () => history.push(`/accounts/${account.id}/`);
+
   return (
-    <div className='single-account-container'>
+    <div
+      className='single-account-container'
+      onClick={goToAccount}
+    >
       <HotswapInput
+        type='textarea'
         contents={name}
+        maxLength={100}
         setContents={hotswapSetName}
         onSubmitConstructor={hotswapSubmitName}
       />
