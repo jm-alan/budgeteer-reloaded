129b899bec17bbcbc065950217815b558dd0ab21
Joshua <j.maxwell.alan@gmail.com>
2021-07-25 15:53:44 -0400

    remove legacy account getter route

--- backend/router/api/users.js
+++ backend/router/api/users.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import asyncHanlder from 'express-async-handler';
 
-import restoreOrReject from '../../utils/restoreOrReject';
 import createToken from '../../utils/createToken';
 import { User } from '../../db/models';
 import { environment, jwtConfig } from '../../config';
@@ -21,22 +20,5 @@ router.post('/', asyncHanlder(async (req, res) => {
   });
   res.json({ user });
 }));
-
-router.get(
-  '/me/:accountType(personals|communals)/',
-  restoreOrReject,
-  asyncHanlder(async (req, res) => {
-    const { user, params: { accountType } } = req;
-    const accountsArray = await user[`get${accountType.upperCaseFirst()}`]();
-    const accounts = {};
-    for (const account of accountsArray) {
-      const Items = {};
-      const itemsArray = await account.getItems();
-      for (const item of itemsArray) Items[item.id] = item;
-      accounts[account.id] = { ...account.dataValues, Items };
-    }
-    res.json({ accounts });
-  })
-);
 
 export default router;
