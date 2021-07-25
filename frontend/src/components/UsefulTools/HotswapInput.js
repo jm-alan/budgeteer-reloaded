9a1f15697d12112b805751faad3da88acd9a37dd
Joshua <j.maxwell.alan@gmail.com>
2021-07-25 15:53:12 -0400

    wrap hotswap input in single outer div

--- frontend/src/components/UsefulTools/HotswapInput.js
+++ frontend/src/components/UsefulTools/HotswapInput.js
@@ -10,49 +10,55 @@ export default function HotswapInput ({ type = 'input', contents, setContents, o
     setEdit(false);
   };
 
-  return edit
-    ? (
-      <form
-        onSubmit={onSubmitConstructor(on, after)}
-      >
-        {(() => (
-          (
-            type === 'input' && (
-              <input
-                value={contents}
-                onChange={({ target: { value } }) => setContents(value)}
-                required
-                className='hotswap-input'
-              />
-            )
-          ) || (
-            type === 'textarea' && (
-              <textarea
-                value={contents}
-                onChange={({ target: { value } }) => setContents(value)}
-                required
-                className='hotswap-input large'
-              />
-            ))
-        ))()}
-        <button
-          type='submit'
-          disabled={loading}
-        >
-          {loading ? 'Loading...' : 'Done'}
-        </button>
-      </form>
-      )
-    : (
-      <div className='hotswap-input-container'>
-        <div className='hotswap-input-text'>
-          {contents}
-        </div>
-        <button
-          onClick={() => setEdit(true)}
-          className='hotswap-input-edit'
-        >Edit
-        </button>
-      </div>
-      );
+  return (
+    <div className='hotswap-container'>
+      {edit
+        ? (
+          <form
+            onSubmit={onSubmitConstructor(on, after)}
+          >
+            {(() => (
+              (
+                type === 'input' && (
+                  <input
+                    value={contents}
+                    onChange={({ target: { value } }) => setContents(value)}
+                    required
+                    disabled={loading}
+                    className='hotswap-input'
+                  />
+                )
+              ) || (
+                type === 'textarea' && (
+                  <textarea
+                    value={contents}
+                    onChange={({ target: { value } }) => setContents(value)}
+                    required
+                    disabled={loading}
+                    className='hotswap-input large'
+                  />
+                ))
+            ))()}
+            <button
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Done'}
+            </button>
+          </form>
+          )
+        : (
+          <>
+            <div className='hotswap-input-text'>
+              {contents}
+            </div>
+            <button
+              onClick={() => setEdit(true)}
+              className='hotswap-input-edit'
+            >Edit
+            </button>
+          </>
+          )}
+    </div>
+  );
 }
