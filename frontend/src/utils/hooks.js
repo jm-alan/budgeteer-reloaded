80f0ae4dc9fc0c2cec9e360db85cd6be3800baa9
Joshua <j.maxwell.alan@gmail.com>
2021-07-25 15:10:40 -0400

    create useHotswap hook

---
+++ frontend/src/utils/hooks.js
@@ -0,0 +1,24 @@
+import { useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+
+export const useHotswap = (key, initialValue, action) => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState(initialValue);
+  const mutable = useRef({
+    [key]: {
+      value,
+      setValue
+    }
+  });
+  const setConstructor = key => value => {
+    mutable.current[key] = value;
+    mutable.current[key].setValue(value);
+  };
+  const onSubmitConstructor = (key, { value }) => (on, after) => e => {
+    e.preventDefault();
+    on();
+    const toUpdate = { [key]: value };
+    dispatch(action(toUpdate, after));
+  };
+  return [setConstructor(key), onSubmitConstructor(key, mutable.current[key])];
+};
