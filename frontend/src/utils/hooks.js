118e5bb14fa3d6985aff57180a9fc7f0ceef5532
Joshua <j.maxwell.alan@gmail.com>
2021-07-26 23:01:58 -0400

    adjust useHotswap to use internal methods for stability

--- frontend/src/utils/hooks.js
+++ frontend/src/utils/hooks.js
@@ -3,15 +3,15 @@ import { useDispatch } from 'react-redux';
 
 export const useHotswap = (key, initialValue, action, ...necessaryArgs) => {
   const dispatch = useDispatch();
-  const [shouldRebuild, setShouldRebuild] = useState(true);
   const [value, setValue] = useState(initialValue);
-  shouldRebuild && value !== initialValue && setValue(initialValue);
+  const [shouldRebuild, setShouldRebuild] = useState(true);
   const mutable = useRef({ value, setValue });
   const mutableSetter = value => {
     mutable.current.value = value;
     mutable.current.setValue(value);
     setShouldRebuild(false);
   };
+  shouldRebuild && value !== initialValue && mutableSetter(initialValue);
   const onSubmitConstructor = (key, { value }) => (on, after) => e => {
     e.preventDefault();
     on();
