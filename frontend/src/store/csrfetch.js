f86bdec9dbf807ca0a2bf4424168ed98407ebfb1
josh <j.maxwell.alan@gmail.com>
2021-07-28 00:19:32 -0400

    refactor csrfetch as less wasteful default object export

--- frontend/src/store/csrfetch.js
+++ frontend/src/store/csrfetch.js
@@ -1,25 +1,23 @@
 import findCookie from '../utils/findCookie';
 import { SetErrors } from './errors';
 
-class CsrfFetch {
-  constructor () {
-    this.options = [
-      '',
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: '',
-        method: ''
-      }
-    ];
+export default {
+  options: [
+    '',
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: '',
+      method: ''
+    }
+  ],
 
-    this.genericErrors = ['Sorry, something went wrong. Please refresh the page and try again.'];
-  }
+  genericErrors: ['Sorry, something went wrong. Please refresh the page and try again.'],
 
   captureDispatch (dispatch) {
     this.dispatch = dispatch;
-  }
+  },
 
   __preFlight (opts = { url: '', params: null, body: null }, method) {
     this.options[1].headers['XSRF-Token'] = findCookie('XSRF-TOKEN');
@@ -32,46 +30,37 @@ class CsrfFetch {
       for (const key in opts.params) opts.url += `&${key}=${opts.params[key]}`;
     }
     if (opts.body) this.options[1].body = JSON.stringify(opts.body);
-  }
+  },
 
   async __forwardFetch (opts = { url: '', params: null, body: null }, method = 'GET') {
     this.__preFlight(opts, method);
     const res = await window.fetch(...this.options);
     try {
-      if (res.status >= 400) throw await res.json();
-      else {
-        try {
-          return await res.json();
-        } catch (err) {
-          if (res.status === 200) return {};
-          throw res;
-        }
-      }
+      if (res.status > 400) throw await res.json();
+      return await res.json();
     } catch ({ errors }) {
       this.dispatch(SetErrors(errors || this.genericErrors));
       return {};
     }
-  }
+  },
 
   async get (url, params) {
     return await this.__forwardFetch({ url, params });
-  }
+  },
 
   async post (url, body) {
     return await this.__forwardFetch({ url, body }, 'POST');
-  }
+  },
 
   async patch (url, body) {
     return await this.__forwardFetch({ url, body }, 'PATCH');
-  }
+  },
 
   async delete (url, body) {
     return await this.__forwardFetch({ url, body }, 'DELETE');
-  }
+  },
 
   async restoreCSRF () {
     await this.get('/api/csrf/restore/');
   }
-}
-
-export default new CsrfFetch();
+};
