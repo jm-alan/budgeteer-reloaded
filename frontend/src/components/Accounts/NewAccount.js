38664fa2294e36efe3f7a3716608a55188dfe046
Joshua <j.maxwell.alan@gmail.com>
2021-07-31 03:36:01 -0400

    new account creation successful

--- frontend/src/components/Accounts/NewAccount.js
+++ frontend/src/components/Accounts/NewAccount.js
@@ -1,20 +1,77 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { CreateAccount } from '../../store/accounts';
 
 export default function NewAccount () {
   const dispatch = useDispatch();
+
+  const history = useHistory();
 
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [balance, setBalance] = useState('');
 
+  const validateBalance = ({ target: { value } }) => {
+    setBalance(prev => {
+      if (
+        !value ||
+        value.match(/^[0-9]+$/) ||
+        value.match(/^[0-9]+\.[0-9]{0,2}$/)
+      ) return value;
+      return prev;
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch(CreateAccount({ name, balance, description }, history));
+  };
+
   return (
-    <form className='new-account-form'>
-      <input
-        value={name}
-        onChange={({ target: { value } }) => setName(value)}
-        className='new-account-input name'
-      />
+    <form
+      className='new-account-form'
+      onSubmit={onSubmit}
+    >
+      <div className='new-account subcontainer organizer'>
+        <label>
+          Account Name
+        </label>
+        <input
+          required
+          placeholder='My Account'
+          value={name}
+          onChange={({ target: { value } }) => setName(value)}
+          className='new-account-input name'
+        />
+      </div>
+      <div className='new-account subcontainer organizer'>
+        <label className='account-starting-balance-label'>
+          Starting Balance $
+        </label>
+        <input
+          required
+          placeholder='e.g. 1000, 1000.00'
+          value={balance}
+          onChange={validateBalance}
+          className='new-account-input balance'
+        />
+      </div>
+      <div className='new-account subcontainer organizer'>
+        <label>
+          Account Description
+        </label>
+        <textarea
+          required
+          value={description}
+          onChange={({ target: { value } }) => setDescription(value)}
+          className='new-account-input description'
+        />
+      </div>
+      <button className='new-account submit'>
+        Create
+      </button>
     </form>
   );
 }
