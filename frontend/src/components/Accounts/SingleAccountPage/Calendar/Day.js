cea2fb6d4aa1126f3ea8b13cfd34cef46a31ccb8
Joshua <j.maxwell.alan@gmail.com>
2021-08-01 23:48:11 -0400

    create rudimentary calendar and expanding date

--- frontend/src/components/Accounts/SingleAccountPage/Calendar/Day.js
+++ frontend/src/components/Accounts/SingleAccountPage/Calendar/Day.js
@@ -1,12 +1,107 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-export default function Day ({ date }) {
+import { GetItemsByDate, UnloadItemsByDate } from '../../../../store/accounts';
+
+export default function Day ({ weekday, date, calendarRef, balance }) {
   const dispatch = useDispatch();
 
+  const items = useSelector(state => state.accounts.currentItems[date]) ?? null;
+  const currentAccount = useSelector(state => state.accounts.current);
+
+  const [resolvedParentRect, setResolvedParentRect] = useState(false);
+  const [top, setInnerTop] = useState(null);
+  const [bottom, setInnerBottom] = useState(null);
+  const [left, setInnerLeft] = useState(null);
+  const [right, setInnerRight] = useState(null);
+  const [width, setInnerWidth] = useState(null);
+  const [height, setInnerHeight] = useState(null);
+  const [detailMode, setDetailMode] = useState(false);
+
+  const freezeRef = useRef(null);
+
+  // const itemList = items && Object.values(items);
+
+  // if (itemList) {
+  //   for (let i = 0; i < itemList.length; i++) {
+  //     balance += +itemList[i].amount;
+  //   }
+  // }
+
+  const displayDate = date.slice(3, 5);
+
+  const onClickDay = e => {
+    const calendarRect = calendarRef.getBoundingClientRect();
+    e.stopPropagation();
+    setDetailMode(true);
+    setInnerTop(calendarRect.top - 5);
+    setInnerBottom(calendarRect.bottom + 10);
+    setInnerLeft(calendarRect.left);
+    setInnerRight(calendarRect.right);
+    setInnerWidth(calendarRect.width);
+    setInnerHeight(calendarRect.height);
+  };
+
+  useEffect(() => {
+    currentAccount && dispatch(GetItemsByDate(date, currentAccount.id));
+    return () => dispatch(UnloadItemsByDate(date));
+  }, [dispatch, currentAccount, date]);
+
+  useEffect(() => {
+    const tearDown = () => {
+      const parentRect = freezeRef.current.getBoundingClientRect();
+      setDetailMode(false);
+      setInnerTop(parentRect.top);
+      setInnerBottom(parentRect.bottom);
+      setInnerLeft(parentRect.left);
+      setInnerRight(parentRect.right);
+      setInnerWidth(parentRect.width);
+      setInnerHeight(parentRect.height);
+    };
+    if (detailMode) document.addEventListener('click', tearDown);
+    return () => document.removeEventListener('click', tearDown);
+  }, [detailMode, setDetailMode]);
+
+  useEffect(() => {
+    const parentRect = freezeRef.current.getBoundingClientRect();
+    setInnerTop(parentRect.top);
+    setInnerBottom(parentRect.bottom);
+    setInnerLeft(parentRect.left);
+    setInnerRight(parentRect.right);
+    setInnerWidth(parentRect.width);
+    setInnerHeight(parentRect.height);
+    setResolvedParentRect(true);
+  }, []);
+
   return (
-    <div className='calendar-day'>
-      <h1>Placeholder</h1>
+    <div
+      ref={freezeRef}
+      className='calendar-freeze'
+      style={{
+        gridColumnStart: weekday + 1
+      }}
+    >
+      {(resolvedParentRect && items && (
+        <div
+          className={`calendar-day${
+            detailMode
+              ? ' detail'
+              : ''
+          }`}
+          style={{ top, bottom, left, right, width, height }}
+          onClick={onClickDay}
+        >
+          <div className='calendar-date'>{displayDate}</div>
+          <div className='calendar-items'>
+            <h1>Placeholder</h1>
+          </div>
+        </div>
+      )) || (
+        <div className='day-loading'>
+          <div className='calendar-date'>{displayDate}</div>
+          <h1>Loading...</h1>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/Accounts/SingleAccountPage/Calendar/index.js
+++ frontend/src/components/Accounts/SingleAccountPage/Calendar/index.js
@@ -1,7 +1,37 @@
+import { useState, useEffect, useRef } from 'react';
+
+import Day from './Day';
+
 export default function Calendar () {
+  const [, reload] = useState();
+  const [resolvedRect, setResolvedRect] = useState(false);
+
+  const dateRef = useRef(new Date());
+  const calendarRef = useRef(null);
+
+  const forward = () => {
+    const now = dateRef.current.getMonth();
+    dateRef.current.setMonth(now === 11 ? 0 : now + 1);
+    reload();
+  };
+
+  const backward = () => {
+    const now = dateRef.current.getMonth();
+    dateRef.current.setMonth(now === 0 ? 11 : now - 1);
+    reload();
+  };
+
+  const datesKV = Object.entries(dateRef.current.toEnumeratedMonthObject());
+
+  useEffect(() => {
+    setResolvedRect(true);
+  }, []);
+
   return (
-    <div className='calendar-container'>
-      {null}
+    <div ref={calendarRef} className='calendar-container'>
+      {resolvedRect && datesKV.map(kv => (
+        <Day calendarRef={calendarRef.current} key={kv[0]} date={kv[0]} weekday={kv[1]} />
+      ))}
     </div>
   );
 }
