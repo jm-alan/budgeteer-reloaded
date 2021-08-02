7990b17b6036eca940c2d1a6c913dfa1b544f1e1
Joshua <j.maxwell.alan@gmail.com>
2021-08-01 21:33:50 -0400

    create Date.toEnumeratedMonthObject method

--- frontend/src/utils/prototypes.js
+++ frontend/src/utils/prototypes.js
@@ -7,3 +7,15 @@ Date.prototype.toShortUTCString = function () {
       ) || (n >= 10 ? n : `0${n}`))
     .join('/');
 };
+
+Date.prototype.toEnumeratedMonthObject = function () {
+  const date = new Date(Date.UTC(this.getUTCFullYear(), this.getUTCMonth(), 1));
+  const enumerated = {};
+  enumerated[date.toShortUTCString()] = date.getUTCDay();
+  date.setUTCDate(2);
+  while (date.getUTCDate() !== 1) {
+    enumerated[date.toShortUTCString()] = date.getUTCDay();
+    date.setUTCDate(date.getUTCDate() + 1);
+  }
+  return enumerated;
+};
