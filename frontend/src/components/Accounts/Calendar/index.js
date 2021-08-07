ce1cbacfb90f95890a1c7e6f0b0a590ce9f4f62f
Joshua <j.maxwell.alan@gmail.com>
2021-08-07 02:18:36 -0400

    add previous/next month buttons to calendar

--- frontend/src/components/Accounts/Calendar/index.js
+++ frontend/src/components/Accounts/Calendar/index.js
@@ -12,13 +12,13 @@ export default function Calendar () {
   const forward = () => {
     const now = dateRef.current.getMonth();
     dateRef.current.setMonth(now === 11 ? 0 : now + 1);
-    reload();
+    reload(_ => !_);
   };
 
   const backward = () => {
     const now = dateRef.current.getMonth();
     dateRef.current.setMonth(now === 0 ? 11 : now - 1);
-    reload();
+    reload(_ => !_);
   };
 
   const datesKV = Object.entries(dateRef.current.toEnumeratedMonthObject());
@@ -28,10 +28,26 @@ export default function Calendar () {
   }, []);
 
   return (
-    <div ref={calendarRef} className='calendar-container'>
-      {resolvedRect && datesKV.map(kv => (
-        <Day calendarRef={calendarRef.current} key={kv[0]} date={kv[0]} weekday={kv[1]} />
-      ))}
-    </div>
+    <>
+      <div className='calendar-header'>
+        <button
+          className='calendar-control backward'
+          onClick={backward}
+        >
+          Prev
+        </button>
+        <button
+          className='calendar-control forward'
+          onClick={forward}
+        >
+          Next
+        </button>
+      </div>
+      <div ref={calendarRef} className='calendar-container'>
+        {resolvedRect && datesKV.map(kv => (
+          <Day calendarRef={calendarRef.current} key={kv[0]} date={kv[0]} weekday={kv[1]} />
+        ))}
+      </div>
+    </>
   );
 }
