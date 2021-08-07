3af3fa8d939d3c004e419256a7df5cfb7be54599
Joshua <j.maxwell.alan@gmail.com>
2021-08-07 02:18:07 -0400

    reliable resizing for calendar day

--- frontend/src/components/Accounts/Calendar/Day.js
+++ frontend/src/components/Accounts/Calendar/Day.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { GetItemsByDate, UnloadItemsByDate } from '../../../store/accounts';
+import { GetItemsByDate, UnloadItems } from '../../../store/accounts';
 
 export default function Day ({ weekday, date, calendarRef, balance }) {
   const dispatch = useDispatch();
@@ -31,47 +31,70 @@ export default function Day ({ weekday, date, calendarRef, balance }) {
   const displayDate = date.slice(3, 5);
 
   const onClickDay = e => {
-    const calendarRect = calendarRef.getBoundingClientRect();
     e.stopPropagation();
     setDetailMode(true);
-    setInnerTop(calendarRect.top - 5);
-    setInnerBottom(calendarRect.bottom + 10);
-    setInnerLeft(calendarRect.left);
-    setInnerRight(calendarRect.right);
-    setInnerWidth(calendarRect.width);
-    setInnerHeight(calendarRect.height);
   };
 
   useEffect(() => {
-    currentAccount && dispatch(GetItemsByDate(date, currentAccount.id));
-    return () => dispatch(UnloadItemsByDate(date));
-  }, [dispatch, currentAccount, date]);
+    const tearDown = () => setDetailMode(false);
+    const scaleUp = () => {
+      if (detailMode) {
+        const calendarRect = calendarRef.getBoundingClientRect();
+        setInnerTop(calendarRect.top - 5);
+        setInnerBottom(calendarRect.bottom + 10);
+        setInnerLeft(calendarRect.left);
+        setInnerRight(calendarRect.right);
+        setInnerWidth(calendarRect.width);
+        setInnerHeight(calendarRect.height);
+      }
+    };
+    const scaleDown = () => {
+      if (!detailMode) {
+        const parentRect = freezeRef.current.getBoundingClientRect();
+        setInnerTop(parentRect.top);
+        setInnerBottom(parentRect.bottom);
+        setInnerLeft(parentRect.left);
+        setInnerRight(parentRect.right);
+        setInnerWidth(parentRect.width);
+        setInnerHeight(parentRect.height);
+      }
+    };
+    if (detailMode) {
+      scaleUp();
+      window.addEventListener('resize', scaleUp);
+      document.addEventListener('click', tearDown);
+    } else scaleDown();
+    return () => {
+      window.removeEventListener('resize', scaleUp);
+      document.removeEventListener('click', tearDown);
+    };
+  }, [detailMode]);
 
   useEffect(() => {
-    const tearDown = () => {
-      const parentRect = freezeRef.current.getBoundingClientRect();
-      setDetailMode(false);
-      setInnerTop(parentRect.top);
-      setInnerBottom(parentRect.bottom);
-      setInnerLeft(parentRect.left);
-      setInnerRight(parentRect.right);
-      setInnerWidth(parentRect.width);
-      setInnerHeight(parentRect.height);
-    };
-    if (detailMode) document.addEventListener('click', tearDown);
-    return () => document.removeEventListener('click', tearDown);
+    dispatch(GetItemsByDate(date, currentAccount.id));
+    return () => dispatch(UnloadItems(date));
+  }, [dispatch, date]);
+
+  useEffect(() => {
   }, [detailMode, setDetailMode]);
 
   useEffect(() => {
-    const parentRect = freezeRef.current.getBoundingClientRect();
-    setInnerTop(parentRect.top);
-    setInnerBottom(parentRect.bottom);
-    setInnerLeft(parentRect.left);
-    setInnerRight(parentRect.right);
-    setInnerWidth(parentRect.width);
-    setInnerHeight(parentRect.height);
-    setResolvedParentRect(true);
-  }, []);
+    const freezeRect = () => {
+      if (!detailMode) {
+        const parentRect = freezeRef.current.getBoundingClientRect();
+        setInnerTop(parentRect.top);
+        setInnerBottom(parentRect.bottom);
+        setInnerLeft(parentRect.left);
+        setInnerRight(parentRect.right);
+        setInnerWidth(parentRect.width);
+        setInnerHeight(parentRect.height);
+        setResolvedParentRect(true);
+      }
+    };
+    freezeRect();
+    window.addEventListener('resize', freezeRect);
+    return () => window.removeEventListener('resize', freezeRect);
+  }, [detailMode]);
 
   return (
     <div
@@ -81,27 +104,31 @@ export default function Day ({ weekday, date, calendarRef, balance }) {
         gridColumnStart: weekday + 1
       }}
     >
-      {(resolvedParentRect && items && (
-        <div
-          className={`calendar-day${
+      {resolvedParentRect && (
+        (items && (
+          <div
+            className={`calendar-day${
             detailMode
               ? ' detail'
               : ''
           }`}
-          style={{ top, bottom, left, right, width, height }}
-          onClick={onClickDay}
-        >
-          <div className='calendar-date'>{displayDate}</div>
-          <div className='calendar-items'>
-            <h1>Placeholder</h1>
+            style={{ top, bottom, left, right, width, height }}
+            onClick={onClickDay}
+          >
+            <div className='calendar-date'>{displayDate}</div>
+            <div className='calendar-items'>
+              <h1>Placeholder</h1>
+            </div>
           </div>
-        </div>
-      )) || (
-        <div className='day-loading'>
-          <div className='calendar-date'>{displayDate}</div>
-          <h1>Loading...</h1>
-        </div>
-      )}
+        )) || (
+          <div
+            className='day-loading'
+            style={{ top, bottom, left, right, width, height }}
+          >
+            <div className='calendar-date'>{displayDate}</div>
+            <h1>Loading...</h1>
+          </div>
+        ))}
     </div>
   );
 }
