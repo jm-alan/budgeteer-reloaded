2e9e2944fccc6182ec1a7285c2d2253936222fd5
Joshua <j.maxwell.alan@gmail.com>
2021-08-09 00:05:13 -0400

    adjust item fetch to prioritize account

--- backend/router/api/accounts.js
+++ backend/router/api/accounts.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
-import { Op } from 'sequelize';
 
 import restoreOrReject from '../../utils/restoreOrReject';
 
@@ -12,27 +11,23 @@ router.get('/', restoreOrReject, asyncHandler(async (req, res) => {
   res.json({ accounts: (await user.getAccounts()).toMappedObject('id') });
 }));
 
-router.get('/:accountId(\\d+)/items/:date(\\d+)/:month(\\d+)/:year(\\d+)/', restoreOrReject, asyncHandler(async (req, res) => {
-  const { user, params: { accountId, date, month, year } } = req;
+router.get(
+  '/:accountId(\\d+)/items/:effectiveDate(\\d+)/:effectiveMonth(\\d+)/:effectiveYear(\\d+)/',
+  restoreOrReject,
+  asyncHandler(async (req, res) => {
+    const { user, params: { accountId, effectiveDate, effectiveMonth, effectiveYear } } = req;
 
-  res.json({
-    items: await user.getItems({
-      where: {
-        [Op.or]: [
-          {
-            effectiveFullDate: `${date}/${month}/${year}`,
-            recurring: false
-          },
-          {
-            effectiveDate: date,
-            recurring: true
-          }
-        ],
-        accountId
-      }
-    })
-  });
-}));
+    const account = await user.findAccountByPK(accountId);
+    if (!account) return res.status(400).json({ errors: ['An account with that ID belonging to this user was not found in the database.'] });
+
+    res.json({
+      items: [
+        ...(await account.getItemsByExactDate(effectiveMonth, effectiveDate, effectiveYear)),
+        ...(await account.getRecurringItemsByDate(effectiveDate))
+      ].toMappedObject('id')
+    });
+  })
+);
 
 router.post('/', restoreOrReject, asyncHandler(async (req, res) => {
   const { user, body } = req;
@@ -46,7 +41,7 @@ router.patch('/:id(\\d+)/', restoreOrReject, asyncHandler(async (req, res) => {
   const { user, body, params: { id } } = req;
 
   const account = await user.findAccountByPK(id);
-  if (!account) return res.json({ account: null });
+  if (!account) return res.status(400).json({ errors: ['An account with that ID belonging to this user was not found in the database.'] });
 
   for (const key in body) if (!['name', 'balance', 'description'].some(validKey => key === validKey)) delete body[key];
   await account.update(body);
